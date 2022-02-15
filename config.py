456385ba060fbfb55905c4a080ae8c66aeecb593
themousery <matthew@themousery.com>
2022-02-15 12:01:03 -0700

    Added bot

---
+++ config.py
@@ -0,0 +1,1 @@
+discord = "OTQzMjExODM0MjM0NzY1Mzcy.YgvweQ.dtK81VMoE785q_gnfgan6EbojFA"
---
+++ main.py
@@ -0,0 +1,18 @@
+import discord, config, requests, time
+
+def getPrice():
+    r = requests.get("https://api.traderjoexyz.com/priceusd/0xf4625148efa2d3e160399b3ffb22230c9a4544ed")
+    return '$' + str(round(int(r.text) * 0.1 ** 18, 2))
+
+bot = discord.Client()
+
+@bot.event
+async def on_ready():
+    print('logged in as {0.user}'.format(bot))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=getPrice()))
+
+if __name__ == '__main__':
+    bot.run(config.discord)
+    while True:
+        time.sleep(60 * 10)
+        bot.dispatch("on_ready")
