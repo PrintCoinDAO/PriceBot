d32fcc654e5c60b1ee1f9fdaee63000e697255e1
themousery <matthew@themousery.com>
2022-02-22 11:55:20 -0700

    Fixed on_message error

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord, config, requests, time, sys
 
 def getPrice():
+    # Function to get the current USD/PRINT price from Trader Joe XYZ
     r = requests.get("https://api.traderjoexyz.com/priceusd/0xf4625148efa2d3e160399b3ffb22230c9a4544ed")
     return '$' + str(round(int(r.text) * 0.1 ** 18, 2))
 
@@ -9,10 +10,12 @@ bot = discord.Client()
 @bot.event
 async def on_ready():
     print('logged in as {0.user}'.format(bot))
+    # Set status to price on startup
     await bot.change_presence(activity=discord.Game(name=getPrice()))
 
 @bot.event
-async def on_message():
+async def on_message(message):
+    # Update status every time a message is sent
     await bot.change_presence(activity=discord.Game(name=price))
 
 if __name__ == '__main__':
